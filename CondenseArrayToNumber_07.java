ee8e09b5b0bcb5901d6155206767c9e743b69653
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-07-29 09:26:36 +0300

    Add files via upload

---
+++ CondenseArrayToNumber_07.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CondenseArrayToNumber_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbersArr = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+
+
+
+    }
+}
